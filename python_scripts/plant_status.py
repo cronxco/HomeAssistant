7fb8925830447ab1d18d889f1c9acd451e913178
willscottuk <willscottuk@users.noreply.github.com>
2017-12-10 11:40:05 +0000

    Ridiculous typo
    Some might even say I made an 'ass' of myself (and how isn't there a donkey emoji!)

--- python_scripts/plant_status.py
+++ python_scripts/plant_status.py
@@ -2,7 +2,7 @@
 # @Date:   04/09/2017 20:06
 # @Project: Ambassadr Home Automation
 # @Last modified by:   willscott
-# @Last modified time: 10/12/2017 11:15
+# @Last modified time: 10/12/2017 11:38
 
 problemPlants = 0
 allproblemPlants = []
@@ -15,7 +15,7 @@ whichIcon = "mdi:help-circle-outline"
 for entity_id in hass.states.entity_ids('plant'):
     state = hass.states.get(entity_id)
     boolean_entity = entity_id.replace('plant.', 'input_boolean.plant_')
-    boolean_state = ass.states.get(switch_entity)
+    boolean_state = hass.states.get(switch_entity)
     if state.state == 'problem' and switch_state.state == 'on':
         problemPlants = problemPlants + 1
         allproblemPlants.append(state.name)

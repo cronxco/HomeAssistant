e3a4c267458881cd7eac296c2455b4fccf6a07f3
Will Scott <willscottuk@users.noreply.github.com>
2018-01-21 17:17:28 +0000

    Climate UI Part 2

--- python_scripts/climate_status.py
+++ python_scripts/climate_status.py
@@ -2,7 +2,7 @@
 # @Date:   21/01/2018 16:54
 # @Project: Ambassadr Home Automation
 # @Last modified by:   willscott
-# @Last modified time: 21/01/2018 17:02
+# @Last modified time: 21/01/2018 17:13
 
 ########################################
 #
@@ -13,12 +13,12 @@
 
 show_card = True
 
-current = hass.states.get('sensor.heating_temperature')
-target = hass.states.get('sensor.heating_set_temp')
-power = hass.states.get('sensor.heating_heating')
+current = hass.states.sensor.heating_temperature.state
+target = hass.states.sensor.heating_set_temp.state
+power = hass.states.sensor.heating_heating.state
 
 if show_card:
-    hass.states.set('sensor.summary', '', {
+    hass.states.set('sensor.heating', '', {
       'custom_ui_state_card': 'state-card-climate',
       'current': current,
       'target': target,
